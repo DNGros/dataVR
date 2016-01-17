7274065174654d0b9925a7c7192cb94de3034420
David Gros <dgros@earthlink.net>
2016-01-17 05:56:53 -0600

    work on index UI

--- custom_src/index.js
+++ custom_src/index.js
@@ -1,4 +1,4 @@
-
+var parseResults;
 //var chartsToGraph = [];
 jQuery("#upCSV").change(function () {
         // Parse local CSV file
@@ -7,25 +7,24 @@ jQuery("#upCSV").change(function () {
                 header: true,
                 complete: function(results, file) {
                     console.log("This file done:", file, results);
-                    var xD = [], yD =[], zD =[], sD=[];
-                    results.data.forEach(function(row, index){
-                        xD.push(row.x);
-                        yD.push(row.y);
-                        zD.push(row.z);
-                        sD.push(row.s);  
-                    });
-                    //chartsToGraph.push(new ScatterGraph(xD,yD,zD,sD));
-                    //Store it for next page
-                    sessionStorage.xD = JSON.stringify(xD);
-                    sessionStorage.yD = JSON.stringify(yD);
-                    sessionStorage.zD = JSON.stringify(zD);
-                    sessionStorage.sD = JSON.stringify(sD);
-                    $('#VisualizeBtn').show();
+                    
+                    if(results.errors.length == 0){
+                        parseResults = results;
+                        $('.uploadComplete').show();
+                    }
+                    else{
+                        console.log("errs exist " + results.errors);
+                        $('.uploadError').show();
+                        $("#errorOut").text(JSON.stringify(results.errors));
+                    }
+                },
+                error:function(err, file){
+                    $("#errorOut").text(JSON.stringify(err));
                 }
             },
             complete: function() {
                 console.log("All files done!");
-                sessionStorage["chartsToGraph"] = JSON.stringify(chartsToGraph);
+             //   sessionStorage["chartsToGraph"] = JSON.stringify(chartsToGraph);
             }
         });
         
@@ -33,5 +32,25 @@ jQuery("#upCSV").change(function () {
 
 //Click listener for visualize button
 $('#VisualizeBtn').click(function(){
+    console.log($("ul#chartSelect li.active a").attr('href'));
+    switch($("ul#chartSelect li.active a").attr('href')){
+        case "#scatter":
+            var xD = [], yD =[], zD =[], sD=[];
+            parseResults.data.forEach(function(row, index){
+                xD.push(row.x);
+                yD.push(row.y);
+                zD.push(row.z);
+                sD.push(row.s);  
+            });
+            //Store it for next page
+            sessionStorage.xD = JSON.stringify(xD);
+            sessionStorage.yD = JSON.stringify(yD);
+            sessionStorage.zD = JSON.stringify(zD);
+            sessionStorage.sD = JSON.stringify(sD);
+            break;
+    }
+    
+    
+    
     window.location.href = 'visualize.html';
 });
