cab4829bad58d9ec4c85aaf45b79987c01dce4b4
David Gros <dgros@earthlink.net>
2016-01-16 20:18:51 -0600

    make it make scatterplots from csv

--- custom_src/index.js
+++ custom_src/index.js
@@ -1,18 +1,37 @@
+
+//var chartsToGraph = [];
 jQuery("#upCSV").change(function () {
-        alert(jQuery(this).val());
         // Parse local CSV file
         $(this).parse({
             config: {
                 header: true,
                 complete: function(results, file) {
                     console.log("This file done:", file, results);
+                    var xD = [], yD =[], zD =[], sD=[];
+                    results.data.forEach(function(row, index){
+                        xD.push(row.x);
+                        yD.push(row.y);
+                        zD.push(row.z);
+                        sD.push(row.s);  
+                    });
+                    //chartsToGraph.push(new ScatterGraph(xD,yD,zD,sD));
+                    //Store it for next page
+                    sessionStorage.xD = JSON.stringify(xD);
+                    sessionStorage.yD = JSON.stringify(yD);
+                    sessionStorage.zD = JSON.stringify(zD);
+                    sessionStorage.sD = JSON.stringify(sD);
                     $('#VisualizeBtn').show();
                 }
             },
             complete: function() {
                 console.log("All files done!");
+                sessionStorage["chartsToGraph"] = JSON.stringify(chartsToGraph);
             }
         });
         
     });
 
+//Click listener for visualize button
+$('#VisualizeBtn').click(function(){
+    window.location.href = 'visualize.html';
+});
